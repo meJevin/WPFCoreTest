89c17cd042337c1455da5947b9681e5493230fe4
Michael <mejevin@gmail.com>
2021-02-19 21:29:14 +0300

    initial commit

---
+++ WPFAppCore/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WPFAppCore
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Todo: initialize update manager
+        }
+
+        private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Todo: check for updates
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Todo: update app
+        }
+    }
+}
